69941b317c366ed2098e12f447fd5504a0be86ed
atewfik1 <101944472+atewfik1@users.noreply.github.com>
2024-04-23 20:11:29 -0400

    Create Project2.m

---
+++ Project2.m
@@ -0,0 +1,90 @@
+%PartA
+%% 1A
+
+Fs = 8000;
+T = 1/Fs;
+StopTime = 1;
+t = (0:T:StopTime-T)';
+s = sin(2*pi*300*t);
+
+what = 20*sin(2*pi*1000*t);
+
+a = zeros(1, Fs);
+r = .9;
+u = .001;
+
+% Transfer function coefficients
+b0 = 1;
+b1 = a;
+b2 = 1;
+
+a0 = 1;
+a1 = a*r;
+a2 = r^2;
+
+b = [b0, b1, b2];
+a = [a0, a1, a2];
+
+[H, w] = freqz(b, a, 8000, 'whole', Fs);
+
+x = s + what;
+
+% fvtool(x,1);
+
+figure(1);
+subplot(2, 1, 1);
+plot(linspace(-pi, pi, length(s)), (fftshift(abs(fft(s+what)))));
+title('Magnitude Response of s+w');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+subplot(2, 1, 2);
+plot(linspace(-pi, pi, length(s)), fftshift(angle(fft(s+what))));
+title('Phase Response of s+w');
+xlabel('rad/samp');
+ylabel('Phase (Radians)');
+
+figure(2);
+plot(linspace(-pi, pi, length(H)), 20*log10(fftshift(abs(H))));
+title('Magnitude Response of H');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+shat=conv(s+what,ifft(H));
+
+figure(3);
+plot(linspace(-pi, pi, length(shat)), (fftshift(abs(fft(shat)))));
+title('Magnitude Response of shat');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+e = zeros(1, Fs);
+y = zeros(1, Fs);
+
+for i = 3:Fs
+    e(i) = x(i) + a(i) *x(i-1) + x(i-2);
+    y(i) = e(i) - r * a(i) * y(i-1) - r*r*y(i-2);
+
+    a(i+1) = a(i) - u*y(i)*x(i-1);
+end
+
+figure(4);
+plot(linspace(-pi, pi, length(y)), (fftshift(abs(fft(y)))));
+title('Magnitude Response of y');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+figure(5);
+plot(a);
+title('a');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+figure(6);
+plot(linspace(-pi, pi, length(a)), fftshift(abs(fft(a))));
+title('Magnitude Response of a');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+
+
