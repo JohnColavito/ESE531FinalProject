1a5dcdbff02d61af1c3745a0ef0f631215ae7226
John Colavito <colavijs@seas.upenn.edu>
2024-04-25 18:10:46 -0400

    a

--- FinalProject.m
+++ FinalProject.m
@@ -62,28 +62,28 @@ for c = 0:10
 end
 
 %%
-r = .90;
-u = .000001;
+r = .95;
+u = .0000001;
 
-N = 1012;
+Fs = 200;
 T = 1/Fs;
-StopTime = 10;
+StopTime = 500;
 t = (0:T:StopTime-T)';
-s = sin(2*pi*60*t);
+s = sin(2*pi*50*t);
 
-i = 10*sin(2*pi*200*t);
-
+i = 50*sin(2*pi*11*t);
 
 figure(1);
 subplot(2, 1, 1);
-plot(linspace(-pi, pi, length(s)), s+i);
-title('s');
+plot(linspace(0, length(s), length(s)), s+i);
+title('s+i');
 xlabel('sample');
 
 subplot(2, 1, 2);
 plot(linspace(-pi, pi, length(s)), 20*log10(fftshift(abs(fft(s+i)))));
 title('s+i');
 xlabel('sample');
+ylabel('dB');
 
 x = s+i;
 y = zeros(1,length(x));
@@ -91,34 +91,75 @@ e = zeros(1,length(x));
 a = zeros(1,length(x));
 
 for index = 3:length(x)
-    e(index) = x(index) + a(index) * x(index-1) + x(index-2);
-    y(index) = e(index) - r*a(index)*y(index-1) - (r^2)*y(index-2);
 
-    a(index+1) = a(index) - u*y(index)*x(index-1);
-    if a(index+1) > 2 || a(index+1) < -2
-        a(index+1) = 0;
+    e(index) = x(index) + a(index) .* x(index-1) + x(index-2);
+    y(index) = e(index) - r.*a(index).*y(index-1) - (r^2).*y(index-2);
+
+
+    a(index+1) = a(index) - u.*y(index).*x(index-1);
+    if (a(index+1) > 2) || (a(index+1) < -2)
+        a(index+1) = a(index);
     end
+    % u = u/.999;
 end
 
 figure(2);
 subplot(2, 1, 1);
-plot(linspace(-pi, pi, length(y)), y);
-title('Magnitude Response of s+i');
-xlabel('rad/samp');
-ylabel('Magnitude (dB)');
+plot(linspace(0, length(y), length(y)), y);
+title('y');
+xlabel('samp');
 
+% y = conv(x, )
 subplot(2, 1, 2);
-plot(linspace(-pi, pi, length(y)), 20*log10(fftshift(abs(fft(y)))));
+plot(linspace(-pi, pi, length(y(length(y)*.8:length(y)))), 20*log10(fftshift(abs(fft(y(length(y)*.8:length(y)))))));
 title('mag of y');
 xlabel('rad/samp');
 ylabel('dB');
 
 figure(3);
-plot(linspace(-pi, pi, length(a)), fftshift(abs(fft(a))));
+plot(linspace(0, length(a), length(a)), a);
 title('a');
 xlabel('time');
 
+b0 = 1;
+b1 = a(end);
+b2 = 1;
+a0 = 1;
+a1 = a(end)*r;
+a2 = r^2;
+b = [b0, b1, b2];
+a3 = [a0, a1, a2];
+[H, w] = freqz(b, a, 10000, 'whole', Fs);
 figure(4);
+plot(linspace(-pi, pi, length(H)), 20*log10(fftshift(abs(H))));
+title('Magnitude Response of H');
+xlabel('rad/samp');
+ylabel('Magnitude (dB)');
+
+%%
+
+x = s+inter;
+y2 = chirp(t,0,1,250);
+
+for index = 3:length(x)
+
+    e(index) = x(index) + a(index) .* x(index-1) + x(index-2);
+    y(index) = e(index) - r.*a(index).*y(index-1) - (r^2).*y(index-2);
+
+
+    a(index+1) = a(index) - u.*y(index).*x(index-1);
+    if (a(index+1) > 2) || (a(index+1) < -2)
+        a(index+1) = a(index);
+    end
+end
+
+figure(5);
+plot(linspace(-pi, pi, length(y2)), 20*log10(fftshift(abs(fft(y2)))));
+title('mag of y');
+xlabel('rad/samp');
+ylabel('dB');
+
+figure(3);
 plot(linspace(0, length(a), length(a)), a);
 title('a');
 xlabel('time');
